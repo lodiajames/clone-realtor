638e6ba0a0544774b55c6ca258eca16faf46cf68
ibrahima <lodiajames@hotmail.com>
2023-01-24 13:23:35 -0500

    inserting map in the litsing page using leaflet library

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -23,6 +23,7 @@ import "swiper/css/bundle";
 
 import { getAuth } from "firebase/auth";
 import ContactLandLord from "../components/ContactLandLord";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 
 export default function Listing() {
@@ -126,7 +127,21 @@ export default function Listing() {
 
                  
          </div>
-         <div className=" bg-slate-700 w-full h-[200px] lg-[400px] z-10 overflow-x-hidden"></div>
+         <div className=" w-full h-[200px] md:h-[400px] md:mt-0 md:ml-2 mt-6 z-10 overflow-x-hidden">
+                <MapContainer center={[listing.geolocation.lat, listing.geolocation.lng]} zoom={13} scrollWheelZoom={false}
+                style={{height:"100%", width:"100%"}}>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[listing.geolocation.lat, listing.geolocation.lng]}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>,   
+
+         </div>
       </div>
     
        
